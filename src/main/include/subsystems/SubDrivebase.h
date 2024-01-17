d0512311c55193449b516f14f68fcf3b37fb51d2
SaumYazdi <kho0042@mhs.vic.edu.au>
2024-01-17 21:04:12 +1100

    HOPEFULLY fixed drivebase for sim and irl

--- src/main/include/subsystems/SubDrivebase.h
+++ src/main/include/subsystems/SubDrivebase.h
@@ -70,10 +70,10 @@ class SubDrivebase : public frc2::SubsystemBase {
   frc::Translation2d _backLeftLocation{-0.281_m, +0.281_m};
   frc::Translation2d _backRightLocation{-0.281_m, -0.281_m};
 
-  const double FRONT_RIGHT_MAG_OFFSET =   -0.872803;   //-0.629883; //-0.127930 ;
-  const double FRONT_LEFT_MAG_OFFSET =    -0.800049;  //-0.695312; //-0.198730;
-  const double BACK_RIGHT_MAG_OFFSET =    -0.668701;    //-0.831543; //-0.331543;
-  const double BACK_LEFT_MAG_OFFSET =     -0.532715;   //-0.965088; //-0.467041;
+  const double FRONT_RIGHT_MAG_OFFSET = -0.872803;  //-0.629883; //-0.127930;
+  const double FRONT_LEFT_MAG_OFFSET =  -0.800049;  //-0.695312; //-0.198730;
+  const double BACK_RIGHT_MAG_OFFSET =  -0.668701;  //-0.831543; //-0.331543;
+  const double BACK_LEFT_MAG_OFFSET =   -0.532715;  //-0.965088; //-0.467041;
 
   SwerveModule _frontLeft{canivore::DriveBaseFrontLeftDrive, canivore::DriveBaseFrontLeftTurn,
                           canivore::DriveBaseFrontLeftEncoder, FRONT_LEFT_MAG_OFFSET};
