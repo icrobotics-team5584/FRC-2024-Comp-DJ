db1870e26be1f6f6900094ca3121a7a217708733
NightShadow38 <oceanphoenix888@gmail.com>
2024-01-17 20:55:39 +1100

    photonlib works

--- src/main/include/subsystems/SubVision.h
+++ src/main/include/subsystems/SubVision.h
@@ -6,16 +6,20 @@
 
 #include <frc2/command/SubsystemBase.h>
 
+#include <photon/PhotonCamera.h>
+
 class SubVision : public frc2::SubsystemBase {
  public:
   SubVision();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
+  //Will be called periodically whenever the CommandScheduler runs.
   void Periodic() override;
+  
 
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+  photon::PhotonCamera camera{"photonvision_5584"};
+  photon::PhotonPipelineResult result = camera.GetLatestResult();
+
 };
