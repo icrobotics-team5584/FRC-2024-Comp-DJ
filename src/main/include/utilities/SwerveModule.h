c6165edcf825c3c3d5a5bd116e963ad821edebb2
ICRoboticsWorkshop <icroboticsfll@gmail.com>
2024-01-20 11:35:41 +1100

    reorder swervemodule.h #includes

--- src/main/include/utilities/SwerveModule.h
+++ src/main/include/utilities/SwerveModule.h
@@ -1,32 +1,23 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
-// #include <ctre/Phoenix.h>
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/CANcoder.hpp>
 #include <ctre/phoenix6/configs/Configs.hpp>
-
+#include <utilities/ICSparkMax.h>
 #include <frc/controller/PIDController.h>
 #include <frc/controller/ProfiledPIDController.h>
+#include <frc/controller/SimpleMotorFeedforward.h>
 #include <frc/kinematics/SwerveModuleState.h>
-#include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/geometry/Rotation2d.h>
+#include <frc/kinematics/SwerveModulePosition.h>
+#include <frc/simulation/DCMotorSim.h>
 #include <units/time.h>
 #include <units/velocity.h>
 #include <units/voltage.h>
 #include <units/length.h>
 #include <units/angular_velocity.h>
-#include <frc/geometry/Rotation2d.h>
 #include <memory>
-#include <frc/controller/SimpleMotorFeedforward.h>
-#include <frc/kinematics/SwerveModulePosition.h>
 #include <numbers>
-#include <utilities/ICSparkMax.h>
-#include <frc/controller/SimpleMotorFeedforward.h>
-#include <frc/simulation/DCMotorSim.h>
-
 
 class SwerveModule {
  public:
