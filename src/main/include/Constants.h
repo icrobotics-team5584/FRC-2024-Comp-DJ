a9bdfe6bb4c573342aed0853a777255f4dae668a
Gastatouillle <bouchergaston4@gmail.com>
2024-02-09 20:59:57 +1100

    added canid comments

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -8,40 +8,40 @@ namespace canivore {
 namespace canid {
 
   //drivebase IDS used: 1-12
- constexpr int DriveBaseFrontRightDrive = 7;
- constexpr int DriveBaseFrontRightTurn = 8;
- constexpr int DriveBaseFrontRightEncoder = 10;
+ constexpr int DriveBaseFrontRightDrive = 7; //done
+ constexpr int DriveBaseFrontRightTurn = 8; //5 robot
+ constexpr int DriveBaseFrontRightEncoder = 10; //done
 
- constexpr int DriveBaseFrontLeftDrive = 3;
- constexpr int DriveBaseFrontLeftTurn = 6;
- constexpr int DriveBaseFrontLeftEncoder = 9;
+ constexpr int DriveBaseFrontLeftDrive = 3; //done
+ constexpr int DriveBaseFrontLeftTurn = 6; //done
+ constexpr int DriveBaseFrontLeftEncoder = 9; //done
       
- constexpr int DriveBaseBackRightDrive = 5;
- constexpr int DriveBaseBackRightTurn = 2;
- constexpr int DriveBaseBackRightEncoder = 12;
+ constexpr int DriveBaseBackRightDrive = 5; //done
+ constexpr int DriveBaseBackRightTurn = 2; //done
+ constexpr int DriveBaseBackRightEncoder = 12; //done
 
- constexpr int DriveBaseBackLeftDrive = 1;
- constexpr int DriveBaseBackLeftTurn = 4;
- constexpr int DriveBaseBackLeftEncoder = 11;
+ constexpr int DriveBaseBackLeftDrive = 1; //done 
+ constexpr int DriveBaseBackLeftTurn = 4; //8 robot
+ constexpr int DriveBaseBackLeftEncoder = 11; //done
 
 
 //Intake IDS used: 13
-  constexpr int IntakeMotor = 13;
+  constexpr int IntakeMotor = 13; //17 robot
 
 //Shooter IDS used: 14-16
-  constexpr int ShooterMotorMain = 14;
-  constexpr int SecondaryShooterMotor = 15;
-  constexpr int ShooterFeederMotor = 16;
+  constexpr int ShooterMotorMain = 14; //18 robot
+  constexpr int SecondaryShooterMotor = 15; //16 robot
+  constexpr int ShooterFeederMotor = 16; //14 robot
 
 //Arm IDS used: 17-18
-  constexpr int ArmMotor = 17;
+  constexpr int ArmMotor = 17; //19
 
 //Amp/Trap IDS used: 19
-  constexpr int AmpMotor = 19;
+  constexpr int AmpMotor = 19; //20 robot
 
 //Climber IDS used: 20-21
-  constexpr int lClimbMotor = 20;
-  constexpr int rClimbMotor = 21;
+  constexpr int lClimbMotor = 20; //15 robot
+  constexpr int rClimbMotor = 21; //13 robot
 
 }
 
