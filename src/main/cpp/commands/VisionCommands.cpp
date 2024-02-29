364e62c29bce062f86c247ceb2bd975cf6c9d683
NightShadow38 <oceanphoenix888@gmail.com>
2024-02-29 20:53:27 +1100

    visionRotateToZero now updates the camYaw and startingGyroYaw so it will not conflict with translation drive

--- src/main/cpp/commands/VisionCommands.cpp
+++ src/main/cpp/commands/VisionCommands.cpp
@@ -2,41 +2,34 @@
 #include "subsystems/SubDrivebase.h"
 #include "subsystems/SubVision.h"
 
-namespace cmd{
-    using namespace frc2::cmd;
+namespace cmd {
+using namespace frc2::cmd;
 
-/*
-    frc2::CommandPtr VisionRotateToZero(){
-        return Run(
-            []{SubDrivebase::GetInstance().RotateToZero(SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER).value_or(0_deg));},
-            {&SubDrivebase::GetInstance()}
-        );
-    }
-*/
+frc2::CommandPtr VisionRotateToZero() {
+  static units::degree_t camYaw = 0_deg;
+  static units::degree_t startingGyroYaw = 0_deg;
 
-    frc2::CommandPtr VisionRotateToZero(){
+  return (Run(
+      [] {
+        auto result = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER);
 
-        static units::degree_t camYaw = 0_deg;
-        static units::degree_t startingGyroYaw = 0_deg;
+        if (result.has_value()) {
+          camYaw = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER).value_or(0_deg);
+          startingGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
+        }
 
-        return RunOnce(
-            []{
-                camYaw = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER).value_or(0_deg);
-                startingGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
-            }
-        ).AndThen(
-            Run(
-                []{
-                    units::degree_t currentGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
-                    units::degree_t gyroAngleTravelled = currentGyroYaw - startingGyroYaw;
-                    units::degree_t errorAngle = camYaw - gyroAngleTravelled;
+        units::degree_t currentGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
+        units::degree_t gyroAngleTravelled = currentGyroYaw - startingGyroYaw;
+        units::degree_t errorAngle = camYaw - gyroAngleTravelled;
 
-                    SubDrivebase::GetInstance().RotateToZero(errorAngle);
-            }));}
-    
-        
-    frc2::CommandPtr ShootSequence(){
-        return VisionRotateToZero().Until([]{return SubVision::GetInstance().IsOnTarget(SubVision::SPEAKER);});
-    }
+        SubDrivebase::GetInstance().RotateToZero(errorAngle);
+      },
+      {&SubDrivebase::GetInstance()}));
+}
 
-} // namespace cmd
+frc2::CommandPtr ShootSequence() {
+  return VisionRotateToZero().Until(
+      [] { return SubVision::GetInstance().IsOnTarget(SubVision::SPEAKER); });
+}
+
+}  // namespace cmd
