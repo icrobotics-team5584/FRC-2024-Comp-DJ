b0b2d99c64c9c6a9ebc1231b9cacf3eddb4679ae
SaumYazdi <kho0042@mhs.vic.edu.au>
2024-03-20 19:32:51 +1100

    Vision Command Now Saves Drivebase Heading

--- src/main/cpp/commands/VisionCommands.cpp
+++ src/main/cpp/commands/VisionCommands.cpp
@@ -9,21 +9,21 @@ frc2::CommandPtr VisionAlignToSpeaker(frc2::CommandXboxController& controller) {
   static units::degree_t camYaw = 0_deg;
   static units::degree_t startingGyroYaw = 0_deg;
 
-  return Run([] {
-           auto result = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER);
+  return RunOnce([] { startingGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees(); })
+      .AndThen(Run([] {
+        auto result = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER);
 
-           if (result.has_value()) {
-             camYaw =
-                 SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER).value_or(0_deg);
-             startingGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
-           }
+        if (result.has_value()) {
+          camYaw = SubVision::GetInstance().GetSpecificTagYaw(SubVision::SPEAKER).value_or(0_deg);
+          startingGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
+        }
 
-           units::degree_t currentGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
-           units::degree_t gyroAngleTravelled = currentGyroYaw - startingGyroYaw;
-           units::degree_t errorAngle = camYaw - gyroAngleTravelled;
+        units::degree_t currentGyroYaw = SubDrivebase::GetInstance().GetHeading().Degrees();
+        units::degree_t gyroAngleTravelled = currentGyroYaw - startingGyroYaw;
+        units::degree_t errorAngle = camYaw - gyroAngleTravelled;
 
-           SubDrivebase::GetInstance().RotateToZero(errorAngle);
-         })
+        SubDrivebase::GetInstance().RotateToZero(errorAngle);
+      }))
       .AlongWith(SubDrivebase::GetInstance().JoystickDrive(controller, true));
 }
 
