bef3d6fe7f98024bf6ba8cb0b0f71070dc5a9f34
Gastatouillle <bouchergaston4@gmail.com>
2024-08-21 20:05:41 +1000

    close notes auto building

--- src/main/cpp/subsystems/SubAuto.cpp
+++ src/main/cpp/subsystems/SubAuto.cpp
@@ -16,31 +16,50 @@ SubAuto::SubAuto() = default;
 // This method will be called once per scheduler run
 void SubAuto::Periodic() {}
 
-
-//Description of total auton
-frc2::CommandPtr SubAuto::CloseNotesAuto(){
-    //Go from start pos to note 1
-    return RunOnce([this]{pathplanner::PathPlannerAuto("Center to 1").ToPtr();})
-    .AndThen(
-        //Either: go from note 1 to subwoofer or go from note 1 to note 2
-       [this]{return cmd::Either(
-            //Go from note 1 to subwoofer and shoot
-            pathplanner::PathPlannerAuto("1 to Shoot").ToPtr().AndThen([this]{
-                //Then go from subwoofer to note 2
-                pathplanner::PathPlannerAuto("Shoot to 2").ToPtr()
-                    ;})
-            , 
-            pathplanner::PathPlannerAuto("1 to 2").ToPtr().AndThen(
-                                cmd::Either(
-                    pathplanner::PathPlannerAuto("Path if decision yes").ToPtr()
-                    ,
-                    pathplanner::PathPlannerAuto("Path if decision no").ToPtr()
-                    ,
-                    []{return true;} //Selector bool
-                )
-            )
-            ,
-     [] {return SubArm::GetInstance().CheckIfArmHasGamePiece();});})}
+// Description of total auton
+frc2::CommandPtr SubAuto::CloseNotesAuto() {
+  // Go from start pos to note 1
+  return RunOnce([this] { pathplanner::PathPlannerAuto("Center to 1").ToPtr(); })
+      .AndThen(
+          // Either: go from note 1 to subwoofer or go from note 1 to note 2
+          [this] {
+            return cmd::Either(
+                // Go from note 1 to subwoofer and shoot
+                pathplanner::PathPlannerAuto("1 to Shoot")
+                    .ToPtr()
+                    .AndThen([this] {
+                      // Then go from subwoofer to note 2
+                      pathplanner::PathPlannerAuto("Shoot to 2").ToPtr();
+                    })
+                    .AndThen([this] {
+                      // Either: go from note 2 to subwoofer or go from note 2 to note 3
+                      return cmd::Either(
+                          // Either:go from note 2 to subwoofer and shoot. END AUTO or go from 2 to
+                          // 3
+                          pathplanner::PathPlannerAuto("2 to Shoot").ToPtr(),
+                          pathplanner::PathPlannerAuto("2 to 3").ToPtr().AndThen([this] {
+                            return pathplanner::PathPlannerAuto("3 to Shoot").ToPtr().OnlyIf([] {
+                              return SubArm::GetInstance().CheckIfArmHasGamePiece();
+                            });
+                          }),
+                          [] { return SubArm::GetInstance().CheckIfArmHasGamePiece(); });
+                    }),
+                pathplanner::PathPlannerAuto("1 to 2").ToPtr().AndThen(cmd::Either(
+                    pathplanner::PathPlannerAuto("2 to Shoot").ToPtr().AndThen(
+                    pathplanner::PathPlannerAuto("Shoot to 3").ToPtr().OnlyIf([] {
+                        return SubArm::GetInstance().CheckIfArmHasGamePiece();
+                      })
+                    ),
+                    pathplanner::PathPlannerAuto("2 to 3").ToPtr().AndThen([this] {
+                      return pathplanner::PathPlannerAuto("3 to Shoot").ToPtr().OnlyIf([] {
+                        return SubArm::GetInstance().CheckIfArmHasGamePiece();
+                      });
+                    }),
+                    [] { return SubArm::GetInstance().CheckIfArmHasGamePiece(); }  // Selector bool
+                    )),
+                [] { return SubArm::GetInstance().CheckIfArmHasGamePiece(); });
+          });
+}
 
 /* TEMPLATE
     return RunOnce([this]{pathplanner::PathPlannerAuto("firstPath").ToPtr();})
