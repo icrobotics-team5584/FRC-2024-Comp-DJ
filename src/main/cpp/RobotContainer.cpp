83937ec63645131ca457dad89d2a88f389e21b7a
Jared Jin <jared.jj.jin@gmail.com>
2024-09-18 20:31:28 +1000

    testing close notes auto, changed robotcontainer to return closenotesauto and added closenotesauto paths

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -203,7 +203,7 @@ void RobotContainer::ConfigureBindings() {
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
   // return pathplanner::PathPlannerAuto("A Center to 2").ToPtr();
-  return SubAuto::GetInstance().SimpleAuto();
+  return SubAuto::GetInstance().CloseNotesAuto();
   //return SubAuto::GetInstance().SimpleAuto();
   // _autoSelected = _autoChooser.GetSelected();
   // units::second_t delay = _delayChooser.GetSelected() * 1_s;
